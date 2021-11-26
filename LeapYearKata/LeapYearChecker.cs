a4a5c2e1ff45a56b3d25a9bb4082d4076737e0b2
vkovaleva <vkovaleva@voxelgroup.net>
2021-11-26 15:13:10 +0100

    refactor: simplify general solution

--- LeapYearKata/LeapYearChecker.cs
+++ LeapYearKata/LeapYearChecker.cs
@@ -5,10 +5,11 @@
         static void Main(string[] args) { }
         public static string LeapYear(int number)
         {
-            if (number != 2020 && number != 2024 && number != 2028)
-                return "Not Leap Year!";
+            if (number % 4 == 0)
+                return "Leap Year!";
+            return "Not Leap Year!";
             
-            return "Leap Year!";
+            
         }
     }
 }
